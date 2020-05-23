6479a66b536e8cb0bafa7e1741a834c80287b7eb
song <747532760@qq.com>
2020-05-23 15:41:34 +0800

    切换分支

--- HelloGit/src/main/java/com/song/git/HelloGit.java
+++ HelloGit/src/main/java/com/song/git/HelloGit.java
@@ -4,5 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
 	System.out.println("第一次更新");
+        System.out.println("切换分支");
+
     }
 }
